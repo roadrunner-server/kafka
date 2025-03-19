07b970fab9218f48da25da296b937afd2f62a888
Valery Piashchynski <piashchynski.valery@gmail.com>
2025-03-19 20:51:26 +0100

    chore: fix genproto
    Signed-off-by: Valery Piashchynski <piashchynski.valery@gmail.com>

--- plugin.go
+++ plugin.go
@@ -1,6 +1,8 @@
 package kafka
 
 import (
+	_ "google.golang.org/genproto/protobuf/ptype" //nolint:revive,nolintlint
+
 	"github.com/roadrunner-server/api/v4/plugins/v4/jobs"
 	"github.com/roadrunner-server/endure/v2/dep"
 	"github.com/roadrunner-server/errors"
